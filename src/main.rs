c4778b751a95231bd8f4c5625cfbb32847d46335
Simonas Sankauskas <simonas.sank@gmail.com>
2023-04-20 23:56:24 +0300

    spi doesn't work as well. need an adult.

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,13 @@
 #![no_std]
 #![no_main]
 
+use nb;
 use core::time::Duration;
+use embedded_hal::blocking::spi::write;
 use embedded_hal::digital::v2::OutputPin;
-use embedded_hal::prelude::{_embedded_hal_blocking_delay_DelayMs, _embedded_hal_timer_CountDown};
+use embedded_hal::prelude::{_embedded_hal_blocking_delay_DelayMs, _embedded_hal_blocking_spi_Write, _embedded_hal_timer_CountDown};
+use embedded_hal::spi;
+use embedded_hal::spi::FullDuplex;
 use smart_leds::{brightness, hsv::RGB8, SmartLedsWrite};
 use teensy4_bsp::board::T40Resources;
 use teensy4_bsp::board;
@@ -18,11 +22,16 @@ use teensy4_bsp::hal::gpt::ClockSource;
 use teensy4_bsp::hal::timer;
 use teensy4_bsp::pins::{t40, tmm};
 use teensy4_panic as _;
-use ws2812_timer_delay::Ws2812;
+use ws2812_spi::Ws2812;
 use fugit::{ExtU32, Megahertz, NanosDuration};
 use imxrt_hal;
 use imxrt_ral;
 use imxrt_hal::ccm::{self, clock_gate, perclk_clk};
+use imxrt_hal::dma::channel::Channel;
+use imxrt_hal::lpspi::{Lpspi, LpspiError};
+use imxrt_hal as hal;
+use nb::block;
+use teensy4_panic::sos;
 
 const DELAY_MS: u32 = 500;
 
@@ -39,6 +48,7 @@ fn main() -> ! {
         mut gpio2,
         mut gpio3,
         mut gpio4,
+        mut lpspi4,
         // `pins` has objects that represent the physical pins. The object
         // for pin 13 is `p13`.
         pins,
@@ -48,8 +58,6 @@ fn main() -> ! {
         ..
     } = board::t40(instances);
 
-    let mut led = board::led(&mut gpio2, pins.p13);
-
     // Configures the GPT1 timer to run at GPT1_FREQUENCY. See the
     // constants below for more information.
     gpt2.disable();
@@ -57,38 +65,8 @@ fn main() -> ! {
     gpt2.set_clock_source(GPT1_CLOCK_SOURCE);
     let mut delay = timer::Blocking::<_, GPT1_FREQUENCY>::from_gpt(gpt2);
 
-    let mut ccm = unsafe { imxrt_ral::ccm::CCM::instance() };
-
-// Before touching the PERCLK clock roots, turn off all downstream clock gates.
-    clock_gate::PERCLK_CLOCK_GATES.iter().for_each(|loc| loc.set(&mut ccm, clock_gate::OFF));
-
-    // Configure PERCLK to match this frequency:
-    const PERCLK_CLK_FREQUENCY_HZ: u32 = ccm::XTAL_OSCILLATOR_HZ / PERCLK_CLK_DIVIDER;
-    const PERCLK_CLK_DIVIDER: u32 = 24;
-    perclk_clk::set_selection(&mut ccm, perclk_clk::Selection::Oscillator);
-    perclk_clk::set_divider(&mut ccm, PERCLK_CLK_DIVIDER);
-
-// Enable the clock gate for our GPT.
-    clock_gate::gpt_bus::<1>().set(&mut ccm, clock_gate::ON);
-    clock_gate::gpt_serial::<1>().set(&mut ccm, clock_gate::ON);
-
-    // GPT1 counts with this frequency:
-    const GPT1_FREQUENCY_HZ: u32 = PERCLK_CLK_FREQUENCY_HZ / GPT1_DIVIDER;
-    const GPT1_DIVIDER: u32 = 100;
-    const GPT1_CLOCK_SOURCE: ClockSource = ClockSource::HighFrequencyReferenceClock;
-
-    gpt1.set_divider(GPT1_DIVIDER);
-    gpt1.set_clock_source(GPT1_CLOCK_SOURCE);
-
-    let mut count_down = timer::CountDown::<_, GPT1_FREQUENCY>::new(
-        timer::RawCountDown::from_gpt(gpt1)
-    );
-
-    count_down.start(Megahertz::<u32>::from_raw(3).into_duration());
-    // count_down.start(NanosDuration::<u32>::from_raw(33333333));
-
-    let mut pin = gpio2.output(pins.p10);
-    let mut ws = Ws2812::new(count_down, pin);
+    // let mut led = board::led(&mut gpio2, pins.p13);
+    let mut pin = gpio4.output(pins.p2);
 
     const NUM_LEDS: usize = 10;
     let mut data = [RGB8::default(); NUM_LEDS];
@@ -96,23 +74,50 @@ fn main() -> ! {
         data[i] = RGB8::new(0, 255, 0);
     }
 
+    let mut spi: board::Lpspi4 = board::lpspi(
+        lpspi4,
+        board::LpspiPins {
+            pcs0: pins.p10,
+            sck: pins.p13,
+            sdo: pins.p11,
+            sdi: pins.p12,
+        },
+        3_000_000,
+    );
+
+    // let mut channels = hal::dma::channels(
+    //     hal::ral::dma0::DMA0::take().unwrap(),
+    //     hal::ral::dmamux::DMAMUX::take().unwrap(),
+    // );
+    //
+    // let mut tx_channel = channels[8].take().unwrap();
+    // let mut rx_channel = channels[9].take().unwrap();
+    // tx_channel.set_interrupt_on_completion(true);
+    // rx_channel.set_interrupt_on_completion(true);
+    //
+    // let duplex = spi.dma_full_duplex(&mut rx_channel, &mut tx_channel, &mut buffer).unwrap();
+    //
+
+    let mut ws = Ws2812::new(NeuSpi { spi: spi });
+
     loop {
         // led.set_high();
         // delay.delay_ms(DELAY_MS);
         // led.set_low();
         // delay.delay_ms(DELAY_MS);
         // let res = pin.set_high();
+        pin.set_high();
         let res = ws.write(data.into_iter());
         match res {
             Ok(value) => {
-                led.set_high();
+                pin.toggle();
             },
             Err(error) => {
-                led.toggle();
-                delay.delay_ms(DELAY_MS);
+                pin.set_high();
             },
         }
-
+        delay.delay_ms(DELAY_MS);
+        pin.set_high();
     }
 }
 
@@ -128,3 +133,36 @@ const GPT1_CLOCK_SOURCE: ClockSource = ClockSource::HighFrequencyReferenceClock;
 /// ... the root clock is PERCLK_CLK. To configure a GPT1 frequency,
 /// we need a divider of...
 const GPT1_DIVIDER: u32 = board::PERCLK_FREQUENCY / GPT1_FREQUENCY;
+
+struct NeuSpi {
+    pub spi: board::Lpspi4,
+}
+
+impl FullDuplex<u8> for NeuSpi {
+
+    type Error = ();
+
+    fn read(&mut self) -> nb::Result<u8, ()> {
+        let res = self.spi.read_data();
+        match res {
+            None => {
+                return nb::Result::Err(nb::Error::WouldBlock);
+            }
+            Some(value) => {
+                return nb::Result::Ok(value as u8);
+            }
+        }
+    }
+
+    fn send(&mut self, word: u8) -> nb::Result<(), ()> {
+        let res = self.spi.write(&[word]);
+        match res {
+            Ok(_) => {
+                return nb::Result::Ok(())
+            }
+            Err(_) => {
+                return nb::Result::Err(nb::Error::WouldBlock);
+            }
+        }
+    }
+}
